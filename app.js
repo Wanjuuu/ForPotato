0432defc189ecba4ea2f7784a452f43f191aa24b
sw0487 <wanjoo0487@gmail.com>
2025-11-16 21:01:31 +0900

    Render 배포용

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const path = require("path");
 const traceRoute = require("./routes/traceRoute");
 
 const app = express();
-const PORT = 3000;
+app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
 
 // POST 폼 데이터 받기
 app.use(express.urlencoded({ extended: true }));
